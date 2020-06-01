dc56756219a3a384b0ad4c38228b9ff8a1b67b56
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-06-01 22:18:40 +0300

    Change sample rate to the native one.

--- src/input.h
+++ src/input.h
@@ -3,12 +3,12 @@
 /*
  * Audio samples per second.
  */
-#define SAMPLE_RATE 10000
+#define SAMPLE_RATE 44100
 /*
  * Samples read per each get_samples() call.
  * SAMPLE_RATE / OUTPUT_RATE = update interval.
  */
-#define OUTPUT_RATE 1000
+#define OUTPUT_RATE 4410
 
 void get_samples(pa_simple *s, float *out);
 pa_simple *get_pa_simple(const char *source_name);
