fbc68a4d7d1da3b6a743e2fc3bf921a887b9fabf
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-06-03 17:35:46 +0300

    Add a line feed to an error message.

--- src/cli.c
+++ src/cli.c
@@ -12,7 +12,7 @@ struct cli_options parse_cli_options(int argc, char *argv[]) {
         char *end;
         opts.queue_depth = strtol(argv[1], &end, 10);
         if (argv[1][0] == '\0' || *end != '\0') {
-            fprintf(stderr, "QUEUE_DEPTH: expected valid integer");
+            fprintf(stderr, "QUEUE_DEPTH: expected valid integer\n");
             exit(BAD_ARG_CODE);
         }
     }
