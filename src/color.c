edeeec9a9c12a7bbb26124623a07f797b42fa9e7
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-06-09 21:49:03 +0300

    Do not check for overflow when it is impossible.

--- src/color.c
+++ src/color.c
@@ -1,20 +1,8 @@
 #include "color.h"
 
-static void overflow(struct color *cp) {
-    for (int i = 0; i < 3; ++i) {
-        if (cp->components[i] > 255) {
-            cp->components[i] = 255;
-        }
-        if (cp->components[i] < 0) {
-            cp->components[i] = 0;
-        }
-    }
-}
-
 struct color color_in_between(struct color c1, struct color c2, float value) {
     for (int i = 0; i < 3; ++i) {
         c1.components[i] += (c2.components[i] - c1.components[i]) * value;
     }
-    overflow(&c1);
     return c1;
 }
