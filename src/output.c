17f2be11ba4ad5ab5a8357bc3c7f13cfd3d7b86d
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-06-09 13:01:26 +0300

    Print some debug info if NDEBUG is not defined.

--- src/output.c
+++ src/output.c
@@ -3,6 +3,14 @@
 #include "output.h"
 
 void set_bg(struct color bg) {
+#ifndef NDEBUG
+    printf(
+        "#%02X%02X%02X\n",
+        bg.components[0],
+        bg.components[1],
+        bg.components[2]
+    );
+#endif
     printf(
         "\033]11;#%02X%02X%02X\007",
         bg.components[0],
