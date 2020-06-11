7c8a84739bf2723b56e2069c5b15ef17ebc50d88
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-06-11 20:31:38 +0300

    Change the output type if stdout is not a tty.

--- src/output.c
+++ src/output.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include "output.h"
 
 void set_bg(struct color bg) {
-#ifndef NDEBUG
-    printf(
-        "#%02X%02X%02X\n",
-        bg.components[0],
-        bg.components[1],
-        bg.components[2]
-    );
-#endif
-    printf(
-        "\033]11;#%02X%02X%02X\007",
-        bg.components[0],
-        bg.components[1],
-        bg.components[2]
-    );
+    if (isatty(fileno(stdout))) {
+        printf("\033]11;#%02X%02X%02X\007",
+               bg.components[0], bg.components[1], bg.components[2]);
+    } else {
+        printf("#%02X%02X%02X\n",
+               bg.components[0], bg.components[1], bg.components[2]);
+    }
     fflush(stdout);
 }
