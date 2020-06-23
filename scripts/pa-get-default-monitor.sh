507c2a87f0a840a9280abd356872e64a547ae641
Sean Ling <sean_ling@kiipo.com>
2020-06-24 00:08:46 +0800

    remove whitespace

--- scripts/pa-get-default-monitor.sh
+++ scripts/pa-get-default-monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pacmd list-sources | \
-grep -B 1 '^.name: <.*\.monitor>' | \
+grep -B1 '^.name: <.*\.monitor>' | \
 awk '/\* index/{getline;monitor=$0;print monitor} /[^\*] index/{getline;fallback=$0;if(monitor==""){print fallback}}' | \
-head -n 1 | \ 
+head -n1 | \
 sed -E -e 's/^.name: <(.*)>$/\1/'
