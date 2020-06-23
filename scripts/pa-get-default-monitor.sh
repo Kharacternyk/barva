8475269d675df44a37304b1172d866d26e6ee770
Sean Ling <aegis965@gmail.com>
2020-06-24 00:32:42 +0800

    move evaluation to END & fallback to first source in list

--- scripts/pa-get-default-monitor.sh
+++ scripts/pa-get-default-monitor.sh
@@ -2,6 +2,6 @@
 
 pacmd list-sources | \
 grep -B1 '^.name: <.*\.monitor>' | \
-awk '/\* index/{getline;monitor=$0;print monitor} /[^\*] index/{getline;fallback=$0;if(monitor==""){print fallback}}' | \
+awk '/\* index/{getline;monitor=$0} /[^\*] index: [01]/{getline;fallback=$0} END {print (monitor=="") ? fallback : monitor}' | \
 head -n1 | \
 sed -E -e 's/^.name: <(.*)>$/\1/'
