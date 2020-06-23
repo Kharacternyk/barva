c22d22b1e5dc2b97e18eccc85189fb60b7fedc8a
Sean Ling <aegis965@gmail.com>
2020-06-24 00:57:26 +0800

    refactor

--- scripts/pa-get-default-monitor.sh
+++ scripts/pa-get-default-monitor.sh
@@ -2,5 +2,5 @@
 
 pacmd list-sources | \
 grep -B1 '^.name: <.*\.monitor>' | \
-awk '/\* index/{getline;monitor=$0} /[^\*] index/{getline;if(fallback==""){fallback=$0}} END {print (monitor=="") ? fallback : monitor}' | \
+awk '/\* index/ {getline;monitor=$0} /[^\*] index/ {getline;if(fallback==""){fallback=$0}} END {print (monitor=="") ? fallback : monitor}' | \
 sed -E -e 's/^.name: <(.*)>$/\1/'
