7ea35f10d284e7c9e630cea53da87a629b8c0705
Sean Ling <aegis965@gmail.com>
2020-06-24 00:35:43 +0800

    remove head command

--- scripts/pa-get-default-monitor.sh
+++ scripts/pa-get-default-monitor.sh
@@ -3,5 +3,4 @@
 pacmd list-sources | \
 grep -B1 '^.name: <.*\.monitor>' | \
 awk '/\* index/{getline;monitor=$0} /[^\*] index: [01]/{getline;fallback=$0} END {print (monitor=="") ? fallback : monitor}' | \
-head -n1 | \
 sed -E -e 's/^.name: <(.*)>$/\1/'
