864be6ca81100650ff7a96e642104227f00b7923
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2021-01-22 23:27:40 +0200

    Wrap the frontend with exception handling.

--- barva/frontends/fft_dumb.py
+++ barva/frontends/fft_dumb.py
@@ -1,8 +1,14 @@
 from backend import Backend
+from fire.core import FireError
 from numpy.fft import fft
+from typeguard import check_argument_types
 
 
-def fft_dumb(*, n=5, window_size=1 / 24):
-    backend = Backend(window_size)
-    for samples in backend:
-        print(" ".join(f"{s:.4f}" for s in abs(fft(samples, n))))
+def fft_dumb(n: int = 5, window_size: float = 1 / 24):
+    try:
+        check_argument_types()
+        backend = Backend(window_size)
+        for samples in backend:
+            print(" ".join(f"{s:.4f}" for s in abs(fft(samples, n))))
+    except Exception as e:
+        raise FireError(str(e))
