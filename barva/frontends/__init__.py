141b0e5663a2436f15293628f83a32fc9778025c
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2021-01-23 00:00:58 +0200

    Reimplement the original barva visualization.

--- barva/frontends/__init__.py
+++ barva/frontends/__init__.py
@@ -1,1 +1,2 @@
 from .fft_dumb import fft_dumb
+from .pulsar import pulsar
---
+++ barva/frontends/pulsar.py
@@ -0,0 +1,39 @@
+from collections import deque
+from math import log
+
+from backend import Backend
+from fire.core import FireError
+from typeguard import check_argument_types
+
+INERTIA = 0.9999
+EPSILON = 1e-6
+
+
+def weighted_root_mean_square(seq):
+    sum_ = 0
+    weight = (1 - INERTIA) / (1 - INERTIA ** len(seq))
+    for x in seq:
+        sum_ += x * x * weight
+        weight *= INERTIA
+    return sum_ ** (1 / 2)
+
+
+def change_bg(value):
+    switchfg = f"\x1b[38;2;{round(value*255)};0;0m"
+    blocks = "████"
+    switchnull = "\x1b[0m"
+    print(switchfg + blocks + switchnull)
+
+
+def pulsar(fps: float = 30):
+    try:
+        check_argument_types()
+        backend = Backend(1 / fps)
+
+        length = int(log(EPSILON) / log(INERTIA))
+        queue = deque((0,) * length, maxlen=length)
+        for samples in backend:
+            queue.extendleft(samples)
+            change_bg(weighted_root_mean_square(queue))
+    except Exception as e:
+        raise FireError(str(e))
